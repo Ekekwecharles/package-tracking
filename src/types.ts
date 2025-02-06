bf46104ec9523d6c5a207b949119bc8ba0768cef
Charlessnow <ekekwecharles50@gmail.com>
2025-02-06 14:22:10 +0100

    fix(types): Fix trac king data typing

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,8 @@
-// export interface TrackingData {
-//   code: string;
-//   destination: string;
-//   destinationCoords: { lat: number; lng: number };
-//   currentPosition: {
-//     lat: number;
-//     lng: number;
-//   };
-//   progress: number;
-// }
-
 export interface TrackingData {
-  currentPosition: { lat: number; lng: number };
+  code: string;
   destination: string;
   destinationCoords: { lat: number; lng: number };
-  startPosition?: { lat: number; lng: number };
+  currentPosition: { lat: number; lng: number };
+  startPosition: { lat: number; lng: number };
+  progress: number;
 }
